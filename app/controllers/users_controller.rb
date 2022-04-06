09199dd01ea5056510ed18224b03e4e7f3863e6b
Scarlett Eller <rq6715@gmail.com>
2022-04-06 16:16:23 -0400

    first migration, sessions controller created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    def create 
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created 
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :username, :password, :password_confirmation, :email)
+    end
+
+    def find_user 
+        User.find_by(id: session[:user_id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
 get '/hello', to: 'application#hello_world'
 
 get '*path',
