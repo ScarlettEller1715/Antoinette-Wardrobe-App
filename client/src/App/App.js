a6faeb88cb6994ada0c6b6d742b5384ccb119bf4
Scarlett Eller <rq6715@gmail.com>
2022-04-06 15:35:26 -0400

    git issue resolution

---
+++ client/src/App/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from '../home/home';
+import Login from '../Login/login';
+import CreateAccount from "../createAccount/createAccount";
+import Account from "../account/account";
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/createaccount">
+            <CreateAccount />
+          </Route>
+
+          <Route path="/account">
+            <Account />
+          </Route>
+
+          <Route path="/">
+            <Home count={count} />
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/Login/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Login() {
+    return (
+        <h1>Login screen</h1>
+    )
+}
+
+export default Login;
