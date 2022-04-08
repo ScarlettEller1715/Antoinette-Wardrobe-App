4245539ce7491493e2c6be04a11cc30a30b93335
Scarlett Eller <rq6715@gmail.com>
2022-04-08 15:55:45 -0400

    Piece ID passed through Link State

--- client/src/Piece Page/piecePage.js
+++ client/src/Piece Page/piecePage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import PageHeader from "../pageHeader/pageHeader";
 
 function PiecePage({ user }) {
+    
+    const location = useLocation();
+    const pieceId = location.state
+
+    
+
     return(
         <React.Fragment>
             <PageHeader user={user} />
