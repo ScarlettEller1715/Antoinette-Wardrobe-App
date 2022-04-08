78925cab8b1d2a9edf63d88f010966817cb27504
Scarlett Eller <rq6715@gmail.com>
2022-04-08 15:22:53 -0400

    successfully rendered piece images

--- client/src/Piece Card/pieceCard.js
+++ client/src/Piece Card/pieceCard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import './pieceCard.css'
+
 function PieceCard({ piece }) {
+    const images = require.context('../../img', true)
 
     return (
         <React.Fragment>
-            <h3>{piece.name}</h3>
-            <p>Wear on {piece.weather.toLowerCase()} days</p>
-            <Link to="/piecepage">See details</Link>
+            <div className="pieceCard">
+                <h3>{piece.name}</h3>
+                <img className="pieceImage" src={images('./' + piece.image_filename)}/>
+                <p>Wear on {piece.weather.toLowerCase()} days</p>
+                <Link to="/piecepage">See details</Link>
+            </div>
         </React.Fragment>
     )
 }
