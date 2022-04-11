3c1be96246a7efed23b24ec7c08db9b926893b67
Scarlett Eller <rq6715@gmail.com>
2022-04-11 17:14:24 -0400

    added basic crud methods to Pieces controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -5,4 +5,19 @@ class PiecesController < ApplicationController
         render json: piece
     end
 
+    def create
+        piece = @current_user.pieces.create!(:pieces_params)
+        render json: piece, status: :created
+    end
+
+    def destroy
+        piece = Piece.find(params[:id])
+        piece.destroy
+        head :no_content
+    end
+
+    private
+    def pieces_params
+        params.permit(:name, :image_filename, :piece_type, :weather, :formality, :color, :clean)
+    end
 end
