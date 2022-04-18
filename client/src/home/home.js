5c05be2413f7b55a322fda7f8593deb3efb947a6
Scarlett Eller <rq6715@gmail.com>
2022-04-18 16:11:34 -0400

    styled Homepage button and text

--- client/src/home/home.js
+++ client/src/home/home.js
@@ -14,14 +14,16 @@ function Home ({ user }) {
                 <div className='homepageText'>
                     <h1>Reign over your wardrobe.</h1>
                     <div className="homepageTextBox">
-                        <h2>Bienvenue à Antoinette,</h2>
+                        <h2 className='textboxTitle'>Bienvenue à Antoinette,</h2>
                         <p>wardrobe organizer at your service. 
                             We offer a bird’s eye view of your closet, 
                             displaying all it's contents in one convenient place. 
                             Discover outfits you’ve never considered, find the hidden 
                             gems you never wear enough, donate the pieces you never wear.</p>
-                        <h2>We can't wait to see what you'll wear next.</h2>
-                        <Link to={user ? "/wardrobe" : "/createaccount"}>Let's begin...</Link>
+                        <h2 className='finalGreeting'>We can't wait to see what you'll wear next.</h2>
+                        <div className='beginButton'>
+                            <Link to={user ? "/wardrobe" : "/createaccount"} className="homepageLink">Let's begin...</Link>
+                        </div>
                 </div>
                 </div>
                 <img src={homepageImage} />
