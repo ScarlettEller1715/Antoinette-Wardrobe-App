b653c4cfcea4641f55a8b3a74a359ac80894f226
Scarlett Eller <rq6715@gmail.com>
2022-04-19 15:27:58 -0400

    Added Logout Wardrobe Copy

--- client/src/LogoutWardrobe/logoutWardrobe.js
+++ client/src/LogoutWardrobe/logoutWardrobe.js
@@ -1,12 +1,22 @@
 import React from "react";
 import PageHeader from "../pageHeader/pageHeader";
 
+import logoutWImage from "../img/logoutWardrobe.PNG"
+
 function LogoutWardrobe({ user }) {
     
     return (
         <React.Fragment>
             <PageHeader user={user} />
-            <h1>This the Wardrobe Page if you're logged out</h1>
+            <img src={logoutWImage} className="logoutWardrobeImage" />
+            <h1>An Introduction to our Services</h1>
+            <p>Antoinette allows you to upload a personalized profile for every piece of clothing in your closet. This includes a name, image, 
+                details of it's appearance, what occasions to which it can be worn and in what kind of weather. These pieces will appear in cards
+                like the one featured, which can be filtered by a variety of traits. Clicking on the piece's name will display further details, while 
+                clicking the laundry basket will place it into your digital laundry. This feature allows you to keep track of what is ready to wear,
+                so you'll always know what is at your disposal. With our services at your disposal, you'll have the perfect guide for what to wear
+                for any circumstance. 
+            </p>
         </React.Fragment>
     )
 }
--- client/src/Nav/nav.js
+++ client/src/Nav/nav.js
@@ -8,12 +8,13 @@ function Nav({ user }) {
     const createAccountOrUsername = (user ? <p>Welcome {user.name}!</p> : <Link to="/createaccount" className="navLink">Create Account</Link>)
     const logInOrAccount = (user ? <Link to="/account" className="navLink">Your Account</Link> : <Link to="/login" className="navLink">Log In</Link>)
     const showLaundry = (user ? <Link to="/laundry" className="navLink">Dirty Laundry</Link> : null)
+    const handleWardrobe = (user ? <Link to="/wardrobe" className="navLink">Your Wardrobe</Link> : <Link to="/wardrobe" className="navLink">What We Do</Link>)
 
     return (
         <React.Fragment>
             <div className="navbar">
                 <Link to="/" className="navLink">Home</Link>
-                <Link to="/wardrobe" className="navLink">Your Wardrobe</Link>
+                {handleWardrobe}
                 {showLaundry}
                 {logInOrAccount}
                 {createAccountOrUsername}
