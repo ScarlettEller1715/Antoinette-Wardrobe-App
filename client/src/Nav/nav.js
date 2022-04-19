fa85eab6aee5dc1def3d57fb3a0f856a20a7c037
Scarlett Eller <rq6715@gmail.com>
2022-04-19 14:22:23 -0400

    removed Laundry from loggedout Navbar

--- client/src/Nav/nav.js
+++ client/src/Nav/nav.js
@@ -6,16 +6,15 @@ import "../Nav/nav.css"
 function Nav({ user }) {
 
     const createAccountOrUsername = (user ? <p>Welcome {user.name}!</p> : <Link to="/createaccount" className="navLink">Create Account</Link>)
-
-
     const logInOrAccount = (user ? <Link to="/account" className="navLink">Your Account</Link> : <Link to="/login" className="navLink">Log In</Link>)
+    const showLaundry = (user ? <Link to="/laundry" className="navLink">Dirty Laundry</Link> : null)
 
     return (
         <React.Fragment>
             <div className="navbar">
                 <Link to="/" className="navLink">Home</Link>
                 <Link to="/wardrobe" className="navLink">Your Wardrobe</Link>
-                <Link to="/laundry" className="navLink">Dirty Laundry</Link>
+                {showLaundry}
                 {logInOrAccount}
                 {createAccountOrUsername}
             </div>
