894126e8a2d9eb04853b4cf4c97133503756f584
Scarlett Eller <rq6715@gmail.com>
2022-04-20 14:55:26 -0400

    Added Piece Validations

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,5 +1,10 @@
 class Piece < ApplicationRecord
     belongs_to :user
 
-    has_one_attached :clothing_image
+    has_one_attached :clothing_image, dependent: :destroy
+
+    validates :name, presence: true
+    validates :piece_type, presence: true
+    validates :weather, presence: true
+    validates :formality, presence: true
 end
